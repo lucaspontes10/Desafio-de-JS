ca2bc433693a73eaaea914a79552e756a821846f
lucaspontes10 <lucaspontesaraujo8@gmail.com>
2024-02-27 14:22:50 -0300

    Desafio de Java if else

---
+++ Desafio_if_else.js
@@ -0,0 +1,35 @@
+const a = 10;
+const b = 28;
+const operação = '+'; // + - * / %
+let resultado ;
+
+
+if (operação === '*')
+{
+(resultado = a * b)
+}
+else if (operação === '+')
+{
+    (resultado = a + b)
+} 
+ else if (operação === '-')
+{
+    (resultado = a - b) 
+}
+else if (operação ==='/')
+{
+    (resultado = a / b) 
+}
+else if (operação === '%')
+{
+    (resultado = a % b) 
+}
+console.log(resultado)
+
+
+
+
+
+
+
+
